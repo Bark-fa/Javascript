a9456cbe729ef0c556985a8bdcb2a8ef7c8b74e1
Bark-fa <71821989+Bark-fa@users.noreply.github.com>
2021-03-07 18:06:14 +0200

    Refactored
    Better execution time, less code

--- Maths/Factorial.js
+++ Maths/Factorial.js
@@ -13,24 +13,25 @@
 
 'use strict'
 
-const calcRange = (num) => {
-  return [...Array(num).keys()].map(i => i + 1)
-}
-
 const calcFactorial = (num) => {
-  if (num === 0) {
-    return 'The factorial of 0 is 1.'
-  }
-  if (num < 0) {
-    return 'Sorry, factorial does not exist for negative numbers.'
-  }
-  if (!num) {
-    return 'Sorry, factorial does not exist for null or undefined numbers.'
-  }
+    
+switch (true) {
+    case num == 0:
+        return 'The factorial of 0 is 1.'
+        break;
+    case num < 0:
+        return 'Sorry, factorial does not exist for negative numbers.'
+        break;
+    case !num:
+        return 'Sorry, factorial does not exist for null or undefined numbers.'
+        break;
+}
   if (num > 0) {
-    const range = calcRange(num)
-    const factorial = range.reduce((a, c) => a * c, 1)
-    return `The factorial of ${num} is ${factorial}`
+      let factorial = 1;
+      for (let i = 1; i <= num; i++) {
+          factorial *= i;
+      }
+      return `The factorial of ${num} is ${factorial}`
   }
 }
 
